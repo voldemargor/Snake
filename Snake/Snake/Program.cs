9d35036492a3d8bce3bb95b0512a8a64d58fa2ae
voldemargor <voldemar.gor@gmail.com>
2019-04-12 14:18:20 +0300

    Snake moves

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Snake
 {
@@ -15,6 +16,7 @@ namespace Snake
             Console.SetWindowSize(1, 1);
             Console.SetBufferSize(80, 25);
             Console.SetWindowSize(80, 25);
+            Console.CursorVisible = false;
 
             // Отрисовка рамочки
             HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
@@ -30,6 +32,16 @@ namespace Snake
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Draw();
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
 
             Console.ReadLine();
         }
