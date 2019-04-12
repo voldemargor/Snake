bbc007ba35c24f14b59054241b5c77b42f9560db
Vladimir Gorshkov <voldemar.gor@gmail.com>
2019-04-12 17:08:30 +0300

    Keys binding added

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -31,11 +31,11 @@ namespace Snake
             }
             else if (direction == Direction.UP)
             {
-                y = y + offset;
+                y = y - offset;
             }
             else if (direction == Direction.DOWN)
             {
-                y = y - offset;
+                y = y + offset;
             }
         }
 
--- Snake/Snake/Snake.cs
+++ Snake/Snake/Snake.cs
@@ -9,6 +9,7 @@ namespace Snake
     class Snake : Figure
     {
         Direction direction;
+
         public Snake(Point tail, int length, Direction _direction)
         {
             direction = _direction;
@@ -39,5 +40,18 @@ namespace Snake
             nextPoint.Move(1, direction);
             return nextPoint;
         }
+
+        public void HandleKey(ConsoleKey key)
+        {
+            if (key == ConsoleKey.LeftArrow)
+                direction = Direction.LEFT;
+            else if (key == ConsoleKey.RightArrow)
+                direction = Direction.RIGHT;
+            else if (key == ConsoleKey.DownArrow)
+                direction = Direction.DOWN;
+            else if (key == ConsoleKey.UpArrow)
+                direction = Direction.UP;
+        }
+
     }
 }
